93135c9db88c931beb522aa2c0d43ef122eb38cf
DWDDover <Declan-Dover@live.nmit.ac.nz>
2025-03-12 10:24:14 +1300

    activities

---
+++ activity.js
@@ -0,0 +1,24 @@
+let a;
+let b;
+let c;
+let d;
+
+const e = 1;
+const f = 2;
+const g = 3;
+const h = 4;
+
+const firstName = 'Declan';
+const lastName = 'Dover';
+const studentStatus = true;
+const country = 'UK';
+const age = 31;
+
+let firstName2, lastName2, studentStatus2, country2, age2;
+
+const myAge = 31;
+const yourAge = 25;
+
+console.log("I am " + myAge + " years old.");
+console.log("You are " + yourAge + " years old.");
+
---
+++ app.js
@@ -0,0 +1,31 @@
+//const firstName = "Declan";
+//const lastName = "Dover";
+// Camel Case naming 
+//const classCode = 'SDV503'
+
+//Invalid vars
+//first-name
+//1_num
+//num_#_1
+
+// Declaring different variables of different types
+
+let firstName = 'Declan';
+let lastName = 'Dover';
+let country = 'UK';
+let age = 31;
+let isStudent = true;
+
+console.log(firstName, lastName, country, age,
+     isStudent
+    );
+
+    const gravity = 9.81;
+    const boilingPoint = 100;
+    const PI = 3.14;
+    console.log(gravity, boilingPoint, PI);
+
+    //variables can also be declaring in one line separated by commas
+
+    let name = 'Declan'; job = 'Student'; live = 'Nelson';
+    console.log(name, job, live);
