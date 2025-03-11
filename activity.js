d02272ed874b3cc199097c7c4fb410acfc6a1f4c
DWDDover <Declan-Dover@live.nmit.ac.nz>
2025-03-12 10:51:16 +1300

    class exercises

--- activity.js
+++ activity.js
@@ -22,3 +22,29 @@ const yourAge = 25;
 console.log("I am " + myAge + " years old.");
 console.log("You are " + yourAge + " years old.");
 
+// TODO
+/*
+data types
+
+1 Primitive data types
+2 Non-primitive data types (Object References)
+
+
+//SECTION - Primitve Data Types
+Means primitive data types are immutable data types
+1 - Numbers (7 or 7.1)
+2 - Strings (Any Data In Single or Double Quotations)
+3 - Booleans (True or False)
+4 - Null (Empty Value)
+5 - Undefined (A Declared variable without a value)
+
+//!SECTION - Primitive Data Types
+Means object references are mutable data types
+1 - Objects
+2 - Functions
+3 - Arrays ( let nums = [1, 2, 3, 4, 5]; )
+*/
+
+let nums = [1, 2, 3, 4, 5];
+nums[2] = 6;
+console.log(nums[2]);
